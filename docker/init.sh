4728c21b5263348e01f48fe9e49a62e499550790
dhimasreza <dhimasreza1989@gmail.com>
2026-05-18 22:04:38 +0700

    Add custom bootstrap and doctype

--- docker/init.sh
+++ docker/init.sh
@@ -1,42 +1,39 @@
-#!bin/bash
-
+#!/bin/bash
 if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     echo "Bench already exists, skipping init"
     cd frappe-bench
     bench start
 else
     echo "Creating new bench..."
+
+    bench init --skip-redis-config-generation frappe-bench --version version-15
+    cd frappe-bench
+
+    # Use containers instead of localhost
+    bench set-mariadb-host mariadb
+    bench set-redis-cache-host redis://redis:6379
+    bench set-redis-queue-host redis://redis:6379
+    bench set-redis-socketio-host redis://redis:6379
+
+    # Remove redis, watch from Procfile
+    sed -i '/redis/d' ./Procfile
+    sed -i '/watch/d' ./Procfile
+
+    bench get-app telephony
+    bench get-app helpdesk https://github.com/dhimasreza/helpdesk --branch main
+
+    bench new-site helpdesk.localhost \
+        --force \
+        --mariadb-root-password Parkee2026!@# \
+        --admin-password Parkee2026! \
+        --no-mariadb-socket
+
+    bench --site helpdesk.localhost install-app telephony
+    bench --site helpdesk.localhost install-app helpdesk
+    bench --site helpdesk.localhost set-config developer_mode 1
+    bench --site helpdesk.localhost set-config mute_emails 1
+    bench --site helpdesk.localhost set-config server_script_enabled 1
+    bench --site helpdesk.localhost clear-cache
+    bench use helpdesk.localhost
+    bench start
 fi
-
-bench init --skip-redis-config-generation frappe-bench --version version-15
-
-cd frappe-bench
-
-# Use containers instead of localhost
-bench set-mariadb-host mariadb
-bench set-redis-cache-host redis://redis:6379
-bench set-redis-queue-host redis://redis:6379
-bench set-redis-socketio-host redis://redis:6379
-
-# Remove redis, watch from Procfile
-sed -i '/redis/d' ./Procfile
-sed -i '/watch/d' ./Procfile
-
-bench get-app telephony
-bench get-app helpdesk --branch main
-
-bench new-site helpdesk.localhost \
---force \
---mariadb-root-password 123 \
---admin-password admin \
---no-mariadb-socket
-
-bench --site helpdesk.localhost install-app telephony
-bench --site helpdesk.localhost install-app helpdesk
-bench --site helpdesk.localhost set-config developer_mode 1
-bench --site helpdesk.localhost set-config mute_emails 1
-bench --site helpdesk.localhost set-config server_script_enabled 1
-bench --site helpdesk.localhost clear-cache
-bench use helpdesk.localhost
-
-bench start
